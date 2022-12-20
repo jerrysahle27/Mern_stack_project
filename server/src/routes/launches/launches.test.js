bbfc7f4ef86c86445225bf9019df87156e49a919
jerrysahle27 <jerrysahle46@gmail.com>
2022-12-20 12:02:42 +0300

    second commit

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,40 @@
+const request = require("supertest");
+const app = require("../../app");
+
+describe("Test GET /launches", () => {
+  test("It should return with 200 success", async () => {
+    const response = await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+describe("Test POST /launches", () => {
+  const completeLaunchData = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-106 f",
+    launchDate: "January 4,2028",
+  };
+  const launchDataWithoutDate = {
+    mission: "USS Enterprise",
+    rocket: "NCC 1701-D",
+    target: "Kepler-106 f",
+  };
+  test("It should return with 201 success", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaunchData)
+      .expect("Content-Type", /json/)
+      .expect(201);
+    const requestDate = new Date(completeLaunchData.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+    expect(requestDate).toBe(responseDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
+  });
+
+  test("It shouls catch missing required properties", () => {
+
+  });
+  test("It shouls catch invalid date", () => {});
+});
